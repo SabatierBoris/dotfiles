314efe9c3b549db699fc72bbebca2368665322f2
SABATIER Boris <boris.sabatier@stormshield.eu>
2017-02-03 13:20:36 +0100

    Improve install dotfile script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,7 +2,25 @@
 
 PWD=$(pwd)
 
-for file in `ls ${PWD} | grep -v $(basename $0)`; do
-    echo "Creating symplink to ${file} in ~"
-    ln -s ${PWD}/${file} ${HOME}/.${file}
-done
+function link {
+	local currentDir=$1
+	local targetDir=$2
+	local prefix=$3
+	for file in `ls ${currentDir} | grep -v $(basename $0) | grep -v README.md`; do
+		local target=${targetDir}/${prefix}${file}
+		local current=${currentDir}/${file}
+		if [ -e ${target} ]; then
+			if ! [ -h ${target} ] && [ "$(readlink ${target})" != "${current}" ]; then
+				if [ -d ${current} ]; then
+					link ${current} ${target} ""
+				else
+					echo "Can't link ${target} debause it's already exists"
+				fi
+			fi
+		else
+			ln -s ${current} ${target}
+		fi
+	done
+}
+
+link $(pwd) ${HOME} "."
